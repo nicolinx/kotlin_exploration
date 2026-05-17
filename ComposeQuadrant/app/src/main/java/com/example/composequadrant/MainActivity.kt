4c5cc898531be67a19f04fb66120668a85502dfa
Nicodemus <nicodemusdemuse@gmail.com>
2026-05-17 19:19:44 +0700

    done ori compose quadrant

--- ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeQuadrantTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ComposeQuadrantScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +39,82 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ComposeQuadrantScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            QuadrantCard(
+                color = Color(0xFFEADDFF),
+                title = "Text composable",
+                desc = "Displays text and follows the recommended Material Design guidelines.",
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantCard(
+                color = Color(0xFFD0BCFF),
+                title = "Image composable",
+                desc = "Creates a composable that lays out and draws a given Painter class object.",
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            QuadrantCard(
+                color = Color(0xFFB69DF8),
+                title = "Row composable",
+                desc = "A layout composable that places its children in a horizontal sequence.",
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantCard(
+                color = Color(0xFFF6EDFF),
+                title = "Column composable",
+                desc = "A layout composable that places its children in a vertical sequence.",
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun QuadrantCard(
+    color: Color, title: String, desc: String,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        color = color,
         modifier = modifier
-    )
+            .fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(16.dp)
+
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Text(
+                text = desc,
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposeQuadrantScreenPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        ComposeQuadrantScreen()
     }
 }
