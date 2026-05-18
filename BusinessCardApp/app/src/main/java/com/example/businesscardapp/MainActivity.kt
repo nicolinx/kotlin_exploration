82e2266671178702f9e1dfc3f690fa477300b32d
Nicodemus <nicodemusdemuse@gmail.com>
2026-05-18 14:38:48 +0700

    done business card project

--- BusinessCardApp/app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ BusinessCardApp/app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -8,23 +8,32 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.businesscardapp.ui.theme.BrandGreen
 import com.example.businesscardapp.ui.theme.BusinessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -81,16 +90,34 @@ fun MainWidget(modifier: Modifier = Modifier) {
             text = "Android Developer Extraordinaire",
             fontSize = 14.sp,
             fontWeight = FontWeight.Bold,
-            color = Color(0xFF235438),
+            color = BrandGreen,
         )
     }
 }
 
 @Composable
 fun ContactInformation(modifier: Modifier = Modifier) {
-    Column(modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "+62 234 1239 1231")
+    Column(
+        modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(space = 16.dp)
+    ) {
+        ContactListTile(text = "+611 (123) 444 555 666", icon = Icons.Default.Phone)
+        ContactListTile(text = "@AndroidDev", icon = Icons.Default.Share)
+        ContactListTile(text = "jen.doe@android.com", icon = Icons.Default.Email)
+    }
+}
+
+@Composable
+fun ContactListTile(text: String, icon: ImageVector) {
+    Row() {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = BrandGreen,
+        )
+        Spacer(Modifier.width(12.dp))
+        Text(text = text)
     }
 }
 
