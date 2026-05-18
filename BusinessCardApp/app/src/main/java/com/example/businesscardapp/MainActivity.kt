7616084990808b1a31be14a21002950948053d8e
Nicodemus <nicodemusdemuse@gmail.com>
2026-05-18 08:32:56 +0700

    done first section

--- BusinessCardApp/app/src/main/java/com/example/businesscardapp/MainActivity.kt
+++ BusinessCardApp/app/src/main/java/com/example/businesscardapp/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscardapp.ui.theme.BusinessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BusinessCardAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    BusinessCardScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +44,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BusinessCardScreen(modifier: Modifier = Modifier) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFDAE4DB))
+    ) {
+        MainWidget(Modifier.weight(0.8f))
+        ContactInformation(Modifier.weight(0.2f))
+    }
+}
+
+@Composable
+fun MainWidget(modifier: Modifier = Modifier) {
+    Column(
+        modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val image = painterResource(R.drawable.android_logo)
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .size(128.dp)
+                .background(color = Color(0xFF172C35))
+        )
+        Spacer(Modifier.height(12.dp))
+        Text(
+            text = "Jennifer Doe",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.W300
+        )
+        Spacer(Modifier.height(4.dp))
+        Text(
+            text = "Android Developer Extraordinaire",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF235438),
+        )
+    }
+}
+
+@Composable
+fun ContactInformation(modifier: Modifier = Modifier) {
+    Column(modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "+62 234 1239 1231")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardScreenPreview() {
     BusinessCardAppTheme {
-        Greeting("Android")
+        BusinessCardScreen()
     }
 }
