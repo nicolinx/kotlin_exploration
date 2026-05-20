a7b02ded006fb9bcfcaa3909690013b6786ef269
Nicodemus <nicodemusdemuse@gmail.com>
2026-05-20 08:49:35 +0700

    add basic trickOrTreat

---
+++ KotlinPlayground/fun_type_and_lambda.kt
@@ -0,0 +1,20 @@
+fun main(){
+  val treatFunction = trickOrTreat(false)
+  val trickFunction = trickOrTreat(true)
+
+  treatFunction()
+  trickFunction()
+}
+
+fun trickOrTreat(isTrick: Boolean): () -> Unit{
+  if(isTrick) return trick
+  else return treat
+}
+
+val trick = {
+  println("No treats!")
+}
+
+val treat : () -> Unit = {
+  println("Have a treat!")
+}
