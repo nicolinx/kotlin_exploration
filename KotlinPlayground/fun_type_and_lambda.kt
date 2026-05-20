9948e4e161bbab7982c2a64ac10060788b16936c
Nicodemus <nicodemusdemuse@gmail.com>
2026-05-20 08:59:34 +0700

    done fun as data types

--- KotlinPlayground/fun_type_and_lambda.kt
+++ KotlinPlayground/fun_type_and_lambda.kt
@@ -1,14 +1,27 @@
 fun main(){
-  val treatFunction = trickOrTreat(false)
-  val trickFunction = trickOrTreat(true)
+  val coins: (Int) -> String = { quantity ->
+    "$quantity quarters"
+  }
+
+  val cupcake: (Int) -> String = {
+    "Have a cupcake"
+  }
+
+  val treatFunction = trickOrTreat(false, coins)
+  val trickFunction = trickOrTreat(true, null)
 
   treatFunction()
   trickFunction()
 }
 
-fun trickOrTreat(isTrick: Boolean): () -> Unit{
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit{
   if(isTrick) return trick
-  else return treat
+  else {
+    if(extraTreat != null) {
+      println(extraTreat(5))
+    }
+    return treat
+  }
 }
 
 val trick = {
