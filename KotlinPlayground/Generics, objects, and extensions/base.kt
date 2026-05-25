f20fb5fe593db5b5140f8f58b452864b1e2a1042
Nicodemus <nicodemusdemuse@gmail.com>
2026-05-25 09:57:03 +0700

    done generics, object, and extensions fundamental

---
+++ KotlinPlayground/Generics, objects, and extensions/base.kt
@@ -0,0 +1,74 @@
+interface ProgressPrintable{
+  val progressText: String
+  fun printProgressBar()
+}
+
+class Quiz: ProgressPrintable{
+  val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+  val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+  val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+  companion object StudentProgress{
+    var total: Int = 10
+    var answered: Int = 3
+  }
+
+  override val progressText: String
+    get() = "${answered} of ${total} answered"
+
+  override fun printProgressBar(){
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(progressText)
+  }
+
+fun printQuiz() {
+    question1.let {
+        println(it.questionText)
+        println(it.answer)
+        println(it.difficulty)
+    }
+    println()
+    question2.let {
+        println(it.questionText)
+        println(it.answer)
+        println(it.difficulty)
+    }
+    println()
+    question3.let {
+        println(it.questionText)
+        println(it.answer)
+        println(it.difficulty)
+    }
+    println()
+  }
+}
+
+// // EXTENSIONS
+// val Quiz.StudentProgress.progressText: String
+//   get() = "${answered} of ${total} answered"
+
+// fun Quiz.StudentProgress.printProgressBar(){
+//     repeat(Quiz.answered) { print("▓") }
+//     repeat(Quiz.total - Quiz.answered) { print("▒") }
+//     println()
+//     println(Quiz.progressText)
+// }
+
+enum class Difficulty{
+  EASY, MEDIUM, HARD
+}
+
+data class Question<T>(
+  val questionText: String,
+  val answer: T,
+  val difficulty: Difficulty,
+)
+
+fun main(){
+  // Quiz().printProgressBar()
+
+  val quiz = Quiz()
+  quiz.printQuiz()
+}
