175f461fc02b9b1fcec8b4eb446cdb8f22ef67ee
Nicodemus <nicodemusdemuse@gmail.com>
2026-05-28 08:00:18 +0700

    done explore superheroes app

--- SuperheroesApp/app/src/main/java/com/example/superheroesapp/HeroesScreen.kt
+++ SuperheroesApp/app/src/main/java/com/example/superheroesapp/HeroesScreen.kt
@@ -1,12 +1,17 @@
 package com.example.superheroesapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -32,10 +37,12 @@ fun HeroCard(hero: Hero, modifier: Modifier = Modifier) {
     shape = MaterialTheme.shapes.medium
   ) {
     Row(
-      modifier = Modifier.padding(16.dp),
+      modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth(),
       verticalAlignment = Alignment.CenterVertically
     ) {
-      Column() {
+      Column(Modifier.weight(1f)) {
         Text(
           stringResource(hero.nameRes),
           style = MaterialTheme.typography.displaySmall
@@ -57,10 +64,23 @@ fun HeroCard(hero: Hero, modifier: Modifier = Modifier) {
   }
 }
 
+@Composable
+fun HeroesScreen(modifier: Modifier = Modifier) {
+  LazyColumn(
+    modifier = modifier,
+    contentPadding = PaddingValues(16.dp),
+    verticalArrangement = Arrangement.spacedBy(8.dp)
+  ) {
+    items(HeroesRepository.heroes) { hero ->
+      HeroCard(hero = hero)
+    }
+  }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun HeroCardPreview() {
+fun HeroScreenPreview() {
   SuperheroesTheme() {
-    HeroCard(HeroesRepository.heroes[0])
+    HeroesScreen()
   }
 }
--- SuperheroesApp/app/src/main/java/com/example/superheroesapp/MainActivity.kt
+++ SuperheroesApp/app/src/main/java/com/example/superheroesapp/MainActivity.kt
@@ -6,42 +6,38 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import com.example.superheroesapp.ui.theme.SuperheroesTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
       SuperheroesTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(
-            name = "Android",
+        Scaffold(
+          modifier = Modifier.fillMaxSize(),
+          topBar = {
+            CenterAlignedTopAppBar(title = {
+              Text(
+                stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+              )
+            })
+          }
+        ) { innerPadding ->
+          HeroesScreen(
             modifier = Modifier.padding(innerPadding)
           )
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  SuperheroesTheme {
-    Greeting("Android")
   }
 }
