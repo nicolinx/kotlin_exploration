c5b342c360d91bc333c3d8e948e897f3762dc237
Nicodemus <nicodemusdemuse@gmail.com>
2026-06-11 09:20:46 +0700

    slicing UI done, add coil asyncImage

--- Amphibians/app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
+++ Amphibians/app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
@@ -4,8 +4,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,10 +19,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.amphibians.R
 import com.example.amphibians.ui.theme.AmphibiansTheme
 
@@ -24,7 +36,56 @@ import com.example.amphibians.ui.theme.AmphibiansTheme
 fun HomeScreen(modifier: Modifier = Modifier) {
     Surface(modifier = modifier) {
 //        LoadingScreen(modifier = Modifier.fillMaxSize())
-        ErrorScreen(modifier = Modifier.fillMaxSize())
+//        ErrorScreen(modifier = Modifier.fillMaxSize())
+        SuccessScreen(modifier = Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun SuccessScreen(modifier: Modifier) {
+    val mocks = listOf("Hello", "Hi", "Hola", "Bonjour")
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(items = mocks, key = { item -> item }) { item ->
+            ItemCard(item, modifier = Modifier.fillMaxWidth())
+        }
+    }
+}
+
+@Composable
+fun ItemCard(
+    item: String,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier) {
+        Column {
+            Text(
+                item, modifier = Modifier.padding(12.dp),
+                style = MaterialTheme.typography.titleMedium
+            )
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/great-basin-spadefoot.png")
+                    .crossfade(true)
+                    .build(),
+                error = painterResource(R.drawable.ic_broken_image),
+                placeholder = painterResource(R.drawable.loading_img),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(16f / 9f),
+                contentScale = ContentScale.Crop,
+                contentDescription = null,
+            )
+            Text(
+                LoremIpsum(30).values.first().replace("\n", " "),
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.padding(12.dp),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
     }
 }
 
