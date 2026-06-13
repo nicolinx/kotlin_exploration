4e6098c84db60eff428a293ebb49167a375a7081
Nicodemus <nicodemusdemuse@gmail.com>
2026-06-13 19:30:55 +0700

    feat: add app bar bookshelf

--- BookshelfApp/app/src/main/java/com/example/bookshelfapp/ui/App.kt
+++ BookshelfApp/app/src/main/java/com/example/bookshelfapp/ui/App.kt
@@ -1,13 +1,40 @@
 package com.example.bookshelfapp.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bookshelfapp.R
 import com.example.bookshelfapp.ui.screens.ListScreen
+import com.example.bookshelfapp.ui.theme.BookshelfAppTheme
 
 @Composable
 fun App() {
-    Scaffold() { innerPadding ->
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { MainTopAppBar() }
+    ) { innerPadding ->
         ListScreen(contentPadding = innerPadding)
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainTopAppBar() {
+    TopAppBar(title = {
+        Text(stringResource(R.string.app_name))
+    })
+}
+
+@Preview
+@Composable
+fun AppPreview() {
+    BookshelfAppTheme() {
+        App()
+    }
+}
