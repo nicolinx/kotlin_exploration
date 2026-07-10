bed0896ef4f5bd4fae7d2cf720604f1cda7c706d
Nicodemus <nicodemusdemuse@gmail.com>
2026-07-10 11:23:37 +0700

    add graddle

--- basic-android-kotlin-compose-training-juice-tracker-views/app/build.gradle.kts
+++ basic-android-kotlin-compose-training-juice-tracker-views/app/build.gradle.kts
@@ -19,6 +19,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("androidx.navigation.safeargs.kotlin")
     id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -53,10 +54,15 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+    composeOptions {
+//        kotlinCompilerExtensionVersion = "1.5.1"
     }
 }
 
 dependencies {
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.core:core-ktx:1.16.0")
@@ -72,4 +78,10 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.material:material:1.12.0")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3")
+    implementation("com.google.accompanist:accompanist-themeadapter-material3:0.28.0")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
