6fafb53413592dca8027b4ccaa8469923d5d1ba1
Nicodemus <nicodemusdemuse@gmail.com>
2026-07-13 08:09:37 +0700

    done interop

--- basic-android-kotlin-compose-training-juice-tracker-compose-starter/app/build.gradle.kts
+++ basic-android-kotlin-compose-training-juice-tracker-compose-starter/app/build.gradle.kts
@@ -71,5 +71,10 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.material:material:1.12.0")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+
+
+    implementation("com.google.android.gms:play-services-ads:22.2.0")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
+
 }
